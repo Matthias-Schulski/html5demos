a815b91bcca670967336c9e9c6400d423991a42c
Matthias-Schulski <67123676+Matthias-Schulski@users.noreply.github.com>
2024-02-04 21:56:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -49,10 +49,10 @@ function spans($list) {
 <section id="wrapper">
   <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=html5demoscom" id="_carbonads_js"></script>
     <header>
-      <h1><abbr>HTML</abbr> Static web-app Matthias/h1>
+      <h1>Static web-app Matthias/h1>
     </header>
     <article>
-      <p><abbr>HTML</abbr> 5 experimentation and demos I've hacked together. Click on the browser support icon or the technology tag to filter the demos (the filter is an <code>OR</code> filter).</p>
+      <p><Pagina van Matthias.</p>
 <?php /*
       <section>
         <a href="http://introducinghtml5.com" id="ih5">
